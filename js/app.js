15c28cb92eb46164da95380d6a5bdd2f8b95eb04
Diego Freire <hig200diego@gmail.com>
2019-05-01 14:14:27 -0300

    .

--- js/app.js
+++ js/app.js
@@ -5,22 +5,42 @@ var keyPressed = document.addEventListener("keydown", function(event){
     myKeyCode.textContent = event.keyCode;
     myLetter.textContent = String.fromCharCode(event.keyCode);
 
+    
     if (event.shiftKey) {
         myLetter.textContent = "Shift";
     }
 
-    if (event.ctrlKey) {
+    else if (event.ctrlKey) {
         myLetter.textContent = "Ctrl";
     }
 
-    if (event.altKey) {
+    else if (event.altKey) {
         myLetter.textContent = "Alt";
     }
 
-    if (event.keyCode == 32) {
+    else if (event.keyCode == 32) {
         myLetter.textContent = "Barra de espaço";
     }
 
-});
+    else if (event.keyCode == 13) {
+        myLetter.textContent = "Enter";
+    }
 
+    else if (event.keyCode == 27) {
+        myLetter.textContent = "Esc";
+    }
+
+    else if (event.keyCode == 8) {
+        myLetter.textContent = "Backspace";
+    }
+
+    else if (event.keyCode == 9) {
+        myLetter.textContent = "Tab";
+    }
+
+    else if (event.keyCode == 20) {
+        myLetter.textContent = "Capslock";
+    } 
+
+});
 
