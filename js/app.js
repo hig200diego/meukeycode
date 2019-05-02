36c308012077b7a35a86e52930256abc0956ae98
Diego Freire <35054039+hig200diego@users.noreply.github.com>
2019-05-02 18:16:35 -0300

    Subsituído 'ifs' por 'switch'

--- js/app.js
+++ js/app.js
@@ -4,6 +4,41 @@ var myLetter = document.querySelector("#myLetter");
 var keyPressed = document.addEventListener("keydown", function(event){
     myKeyCode.textContent = event.keyCode;
     myLetter.textContent = String.fromCharCode(event.keyCode);
+    
+    
+    switch(event.keyCode) {
+        case 32:
+        myLetter.textContent = "Barra de espaço";
+        break;
+
+        case 13:
+        myLetter.textContent = "Enter";
+        break;
+
+        case 27:
+        myLetter.textContent = "Esc";
+        break;
+
+        case 8:
+        myLetter.textContent = "Backspace";
+        break;
+
+        case 9:
+        myLetter.textContent = "Tab";
+        break;
+
+        case 8:
+        myLetter.textContent = "Alt Gr";
+        break;
+
+        case 20:
+        myLetter.textContent = "Capslock";
+        break;
+
+        default:
+        myLetter.textContent = String.fromCharCode(event.keyCode);
+    }
+
 
     
     if (event.shiftKey) {
@@ -18,7 +53,7 @@ var keyPressed = document.addEventListener("keydown", function(event){
         myLetter.textContent = "Alt";
     }
 
-    else if (event.keyCode == 32) {
+    /*else if (event.keyCode == 32) {
         myLetter.textContent = "Barra de espaço";
     }
 
@@ -40,7 +75,8 @@ var keyPressed = document.addEventListener("keydown", function(event){
 
     else if (event.keyCode == 20) {
         myLetter.textContent = "Capslock";
-    } 
+    }
+    */
 
 });
 
